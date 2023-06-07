88ca16f96236c4d2e2ba80d6f06eb995d76d60a9
Christian Seidler <ch.cseidler@gmail.com>
2023-06-06 23:46:53 -0300

    First component

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,5 +10,10 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'indent': ['error', 2],
+    'no-multi-spaces': ['error'],
+    'no-multiple-empty-lines': ['error', { 'max': 1, 'maxBOF': 0, 'maxEOF': 0 }]
   },
-}
+};
---
+++ src/components/items/itemsList.component.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const ItemsListComponent: React.FC<{items: any[]}> = (props) => {
+
+  return (
+    <div>
+      <h3>Items:</h3>
+      <ul>
+        {
+          props.items.map((item, index) => <li key={index}>{item.name}</li>)
+        }
+      </ul>
+    </div>
+  );
+};
