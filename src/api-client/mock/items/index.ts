52ec30bc0939274114d672aad37c749d3bdadc0e
Christian Seidler <ch.cseidler@gmail.com>
2023-06-10 12:22:38 -0300

    Items domain mock API instance

---
+++ src/api-client/mock/items/index.ts
@@ -0,0 +1,12 @@
+import { ItemsApiClientInterface, ItemsApiClientModel, ItemsApiClientOptions } from '../../models/items';
+
+const options: ItemsApiClientOptions = {
+  endpoints: {
+    fetchItems: '/static/mock-data/items/items.json'
+  },
+  mockDelay: 1000,
+};
+
+const itemsApiClient: ItemsApiClientInterface = new ItemsApiClientModel(options);
+
+export { itemsApiClient };
