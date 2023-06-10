cd29b86b0476b5b4a7b490d48e4896b1a9099a72
Christian Seidler <ch.cseidler@gmail.com>
2023-06-10 12:59:09 -0300

    environment variables and configs

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+/// <reference types="react" />
+
+interface ImportMetaEnv {
+    readonly VITE_API_CLIENT: string
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
