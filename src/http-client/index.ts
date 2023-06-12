33aaa06018cff9b674d1c26d5a98886fa3cd3bf6
Christian Seidler <ch.cseidler@gmail.com>
2023-06-12 13:04:45 -0300

    Client factory

---
+++ src/http-client/index.ts
@@ -0,0 +1,19 @@
+import { HttpClientAxios, HttpClientFetch, HttpClientInterface } from './models';
+
+export * from './models';
+
+let _httpClient: HttpClientInterface | undefined = undefined;
+
+export const useHttpClient = () => {
+  if (!_httpClient) {
+    const clientType = 'fetch';
+
+    if (clientType === 'fetch') {
+      _httpClient = new HttpClientFetch();
+    } else if (clientType === 'axios') {
+      _httpClient = new HttpClientAxios();
+    }
+  }
+
+  return _httpClient as HttpClientInterface;
+};
