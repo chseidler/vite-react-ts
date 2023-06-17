a70568f0f6a0872db0c716a2113c68d238fcf207
Christian Seidler <ch.cseidler@gmail.com>
2023-06-17 17:47:46 -0300

    import i18n in main and react suspense

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import './localization/I18n.init';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <React.Suspense fallback="loading">
+      <App />
+    </React.Suspense>
   </React.StrictMode>,
 );
