ea6d8c492d5dbf5739078a927152c47846724255
Christian Seidler <ch.cseidler@gmail.com>
2023-06-22 23:15:04 -0300

    button fix

--- src/components/primitives/buttons/ElButton.tsx
+++ src/components/primitives/buttons/ElButton.tsx
@@ -1,18 +1,49 @@
-interface SomeButtonProps {
-    label: string,
-    disabled: boolean,
-    onClicked: () => any;
+type ElButtonProps = {
+  testid?: string,
+  id: string,
+  label: string,
+  disabled?: boolean,
+  addCss?: string,
+  onClicked: Function,
 }
 
-export function SomeButton (props: SomeButtonProps) {
-  const { label } = props;
+export function SomeButton (props: ElButtonProps) {
+  const { addCss, id, label, onClicked } = props;
+  const testid = props.testid || 'testid-not-set';
   const disabled = props.disabled || false;
 
   const cssClass = (): string => {
-    const result = ['p-6'];
-
+    const result = [
+      'font-bold',
+      'py-1',
+      'px-2',
+      'inline-flex',
+      'justify-center',
+      'rounded-md',
+      'border',
+      'shadow-sm',
+      'focus:outline-none',
+      'focus:ring-2',
+      'focus:ring-offset-2'
+    ];
     if (disabled) {
-      result.push('disabled');
+      result.push(
+        'bg-gray-500',
+        'text-gray-300',
+        'opacity-50',
+        'cursor-not-allowed'
+      );
+    } else {
+      result.push(
+        'bg-blue-500',
+        'text-whit',
+        'hover:bg-blue-400',
+        'focus:ring-blue-300'
+      );
+    }
+
+    if (addCss) {
+      result.push(addCss);
     }
 
     return result.join(' ').trim();
@@ -20,18 +51,20 @@ export function SomeButton (props: SomeButtonProps) {
 
   const handleClick = () => {
     if (!disabled) {
-      props.onClicked();
+      onClicked(id);
     }
   };
 
   return (
-    <button 
-      type='button'
+    <button
+      type="button"
+      aria-label={label}
+      data-testid={testid}
       disabled={disabled}
       className={cssClass()}
       onClick={handleClick}
     >
-      <span className="name">{ label }</span>
+      <span className="name">{label}</span>
     </button>
   );
 }
